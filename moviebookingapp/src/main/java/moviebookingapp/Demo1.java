ca411ed8271437e7a5861bdbaa17e00c5d6061b0
Vemulati Udaykumarreddy <vemudayk@publicisgroupe.net>
2022-04-28 11:47:14 +0530

    addition

---
+++ moviebookingapp/src/main/java/moviebookingapp/Demo1.java
@@ -0,0 +1,13 @@
+package moviebookingapp;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int num1,num2,num3;
+		num1=10;
+		num2=20;
+		num3=num1+num2;
+		System.out.println("num3"+ num3);
+	}
+
+}
